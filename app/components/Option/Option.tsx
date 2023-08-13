cf0dbcf9fcab2fa2bc6653fb0c2dc9018f1fd6ea
Sol <noguerasol22@gmail.com>
2023-08-12 21:31:17 -0300

    fix error interactivity

--- app/components/Option/Option.tsx
+++ app/components/Option/Option.tsx
@@ -10,7 +10,7 @@ const Option = ({ title, onClickOption }: ButtonProps) => {
     <button
       className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
       hover:border-blue-400 focus:bg-blue-100 active:bg-blue-200"
-      onClick={() => onClickOption(title)}
+      //onClick={() => onClickOption(title)}
     >
       <div className="relative flex items-center space-x-4 justify-center">
         <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
